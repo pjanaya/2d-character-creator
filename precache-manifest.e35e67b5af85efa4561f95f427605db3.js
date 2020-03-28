8887b2e780c0781cb8865522c56300b23c398f6c
Pablo Anaya <pjanaya@gmail.com>
2020-03-28 01:06:17 +0000

    Updates

---
+++ precache-manifest.e35e67b5af85efa4561f95f427605db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6046ef4c9d1f420ad7a0f51aa76a25",
+    "url": "/2d-character-creator/index.html"
+  },
+  {
+    "revision": "378f2206efa528840a7e",
+    "url": "/2d-character-creator/static/css/main.f5eee0a0.chunk.css"
+  },
+  {
+    "revision": "6862a0912d6c9a640342",
+    "url": "/2d-character-creator/static/js/2.e2d0c92d.chunk.js"
+  },
+  {
+    "revision": "6dcfec103c0715605dfbaf5f4ff156bc",
+    "url": "/2d-character-creator/static/js/2.e2d0c92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378f2206efa528840a7e",
+    "url": "/2d-character-creator/static/js/main.808aa619.chunk.js"
+  },
+  {
+    "revision": "216f4ed3dd2be8f4a2db",
+    "url": "/2d-character-creator/static/js/runtime-main.3b043e8c.js"
+  }
+]);
